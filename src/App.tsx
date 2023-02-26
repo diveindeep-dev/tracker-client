2812a58e0d21ae4622c8f69c1fa564b2cc05c91b
hakyung <diveindeep.dev@gmail.com>
2023-02-26 16:06:13 +0900

    Implement layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Layout from './layouts';
 import Index from './pages';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Index />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Index />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }

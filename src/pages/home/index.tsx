653be1fdcce15dde8b07e001ef3af276067f065d
hakyung <diveindeep.dev@gmail.com>
2023-08-27 18:49:36 +0900

    Implement infinite scroll with Intersection Observer API

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,15 +6,39 @@ import Tracker from '../../components/Tracker';
 function Home() {
   const [trackerList, setTrackerList] = useState<Tracker[]>([]);
   const [page, setPage] = useState<number>(1);
+  const [target, setTarget] = useState<HTMLDivElement | null>(null);
 
   const getListSet = async () => {
     const { status, data } = await getTrackerListByPageApi(page);
-    setTrackerList(data.trackers);
+    if (status === 200) {
+      if (target && data.trackers.length < 5) {
+        setTarget(null);
+      }
+      setTrackerList((prev) => [...prev, ...data.trackers]);
+    }
   };
 
   useEffect(() => {
     getListSet();
   }, [page]);
+
+  const onIntersect: IntersectionObserverCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        setPage((prev) => prev + 1);
+      }
+    });
+  };
+
+  useEffect(() => {
+    let observer: IntersectionObserver;
+    if (target) {
+      observer = new IntersectionObserver(onIntersect, { threshold: 0.1 });
+      observer.observe(target);
+    }
+
+    return () => observer && observer.disconnect();
+  }, [target]);
 
   const trackers = trackerList.map((tracker: Tracker, i: number) => {
     return <Tracker key={i} tracker={tracker} />;
@@ -24,6 +48,7 @@ function Home() {
     <div>
       <NewTracker />
       <div>{trackers}</div>
+      <div ref={setTarget}>You're up to date</div>
     </div>
   );
 }

ea7e7555de4406704954b050c96f671d874c0a3c
hakyung <diveindeep.dev@gmail.com>
2023-09-09 16:36:51 +0900

    Fix environmnet variables to add prefix(REACT_APP_)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const SERVER_URL: string | undefined =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3001'
-    : process.env.PRODUCT_URL;
+    : process.env.REACT_APP_PRODUCT_URL;
 
 export { SERVER_URL };
